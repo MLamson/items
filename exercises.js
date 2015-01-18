137286ef05d108d984e58048854dec46d75b25ec
Mark Lamson <lamson_mark@yahoo.com>
2015-01-17 19:31:18 -0500

    sum and multiply

--- exercises.js
+++ exercises.js
@@ -85,13 +85,23 @@ function rovarspraket(inputText){
 // ---------------------
 // Define a function sum() and a function multiply() that sums and multiplies (respectively) all the numbers in an array of numbers. For example, sum([1,2,3,4]) should return 10, and multiply([1,2,3,4]) should return 24.
 // ---------------------
+    var a = 0;
+function sum(numToSum){
+    numToSum.forEach(function(numbersAdd){
+    a += numbersAdd;
+    });
 
-function sum(){
-    //...
+    console.log(a);
 }
+    
+    var a = 1;
+function multiply(numToMult){
+    numToMult.forEach(function(numbersMult){
 
-function multiply(){
-    //...
+    a *= numbersMult;
+    });
+
+    console.log(a);
 }
 
 // ---------------------
